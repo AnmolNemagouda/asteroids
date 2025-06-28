d3439e4006a65ead5d929ef82a42f46f47606b66
AnmolNemagouda <anemagou@asu.edu>
2025-06-28 14:18:50 -0700

    C: Added player.py with triangle player charachter, initialized a player object in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
-
+from player import Player
+from circleshape import CircleShape
 def main():
     pygame.init()
     print("Starting Asteroids!")
@@ -9,7 +10,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     newobj = pygame.time.Clock()
     dt=60
-    
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +18,7 @@ def main():
         screen.fill((0, 0, 0))
         a= newobj.tick(60)
         dt = a / 1000.0  # Convert milliseconds to seconds
+        player.draw(screen)
         pygame.display.flip()
 if __name__ == "__main__":
     main()
