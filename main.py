cb039ebdddd06accb1abe4292df33674c3064bb1
AnmolNemagouda <anemagou@asu.edu>
2025-06-28 14:02:52 -0700

    B: Added circleshape and updated player radius in constants, added tick for 60 fps in main

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ def main():
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    newobj = pygame.time.Clock()
+    dt=60
     
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill((0, 0, 0))
+        a= newobj.tick(60)
+        dt = a / 1000.0  # Convert milliseconds to seconds
         pygame.display.flip()
 if __name__ == "__main__":
     main()
